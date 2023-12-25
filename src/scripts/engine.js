1d7bf5ffdbe1b7f46417854910e10029e45d6bf1
Davi Viegas <126213196+davivie@users.noreply.github.com>
2023-12-25 16:12:54 -0300

    commit init
    Jogo da Memoria

---
+++ src/scripts/engine.js
@@ -0,0 +1,70 @@
+const emojis = [
+
+"💕",
+"💕",
+"😘",
+"😘",
+"❤️",
+"❤️",
+"😍",
+"😍",
+"💖",
+"💖",
+"💞",
+"💞",
+"💘",
+"💘",
+"💗",
+"💗",
+
+];
+
+let openCards = []; /**guardar os elemntos que tiverem abrindo */
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1));
+
+for (let i=0; i < emojis.length; i++) {
+    let box = document.createElement ("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").
+    appendChild( box );
+
+}
+
+/* comporatamento de click */
+
+function handleClick(){
+
+    if (openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2 ){
+        setTimeout(checkMatch, 500 );
+        }
+    }
+
+    function checkMatch(){ 
+        if (openCards [0].innerHTML === 
+            openCards[1].innerHTML){
+            openCards [0].classList.add("boxMatch");
+            openCards [1].classList.add("boxMatch");
+        }else{
+            openCards [0].classList.remove("boxOpen");
+            openCards [1].classList.remove("boxOpen");
+        }
+
+        openCards = []; /*resetar vetores */
+
+            if (document.querySelectorAll(" .boxMatch" )
+            .length === emojis.length ){
+                alert("Você Venceu !");
+            }
+
+        }
+    
+
+
